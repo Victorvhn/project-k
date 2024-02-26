1ee34bae1f3a149134f9542e808c54d9fa630ec9
Victor Hugo do Nascimento <victor.hugo.nascimento58@gmail.com>
2024-02-26 10:13:59 -0300

    feat: Add vercel SpeedInsights

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 import './globals.css';
 
@@ -12,6 +13,7 @@ export default function RootLayout({
   return (
     <>
       <Analytics />
+      <SpeedInsights />
       {children}
     </>
   );
